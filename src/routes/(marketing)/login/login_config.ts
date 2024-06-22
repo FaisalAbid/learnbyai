fcec459a952dd1f9a1feccca52e5fdded919c00f
Faisal Abid <faisal.abid@gmail.com>
2024-06-22 15:49:58 -0400

    Update auth view to sign_up and remove Google provider

--- src/routes/(marketing)/login/login_config.ts
+++ src/routes/(marketing)/login/login_config.ts
@@ -2,7 +2,7 @@ import { ThemeSupa } from "@supabase/auth-ui-shared"
 import type { Provider } from "@supabase/supabase-js"
 
 export const oauthProviders = [
-  'google'
+
 ] as Provider[]
 
 // use the css variables from DaisyUI to style Supabase auth template
